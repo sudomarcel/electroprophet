6e209530888807d1a6fffb3f7f122e1de63bc613
Tobias Rustler <Tobias.Rustler@outlook.de>
2023-03-10 15:12:41 -0300

    added function for requesting the energy data, and moved the imports out of the function 'get_weater'

--- prophecy/data.py
+++ prophecy/data.py
@@ -1,29 +1,29 @@
+import requests
+import datetime
+from dateutil.relativedelta import relativedelta
+import pandas as pd
+import os.path
+
 def get_weather(city, years=10, overwrite=False):
-    
+
     '''
-    This function receives the name of a city and a number of years, and returns a dataframe 
+    This function receives the name of a city and a number of years, and returns a dataframe
     with weather data from this city during those past years
     '''
-    
-    import requests
-    import datetime
-    from dateutil.relativedelta import relativedelta
-    import pandas as pd
-    import os.path
-    
+
     path = '/home/caiodamasceno/code/Johnny4good/electroprophet/raw_data/df_' + city.lower() + '_weather.csv'
-    file_exists = os.path.isfile(path) 
-    
+    file_exists = os.path.isfile(path)
+
     if file_exists and not overwrite:
-        
+
         print('Found a file for', city + '.','Importing...')
-        
+
         weather_df = pd.read_csv(path, index_col=0)
-        
+
     else:
-        
+
         print('Creating a new .csv file for', city)
-        
+
         # First we declare the weather parameters. Here we'll be taking all params supported by the API
         weather_params = ['temperature_2m','relativehumidity_2m','dewpoint_2m',
                       'apparent_temperature','pressure_msl','surface_pressure',
@@ -47,7 +47,7 @@ def get_weather(city, years=10, overwrite=False):
 
         # Then we compute the dates used to get the weather data
         ## The API only has data until 9 days ago
-        end_date = (datetime.date.today() - relativedelta(days=8)).strftime('%Y-%m-%d') 
+        end_date = (datetime.date.today() - relativedelta(days=8)).strftime('%Y-%m-%d')
         start_date = (datetime.date.today() - relativedelta(years=years)).strftime('%Y-%m-%d')
 
         # So we make the request to the weather API archive
@@ -60,8 +60,42 @@ def get_weather(city, years=10, overwrite=False):
                                     'timezone': 'auto'}).json()
 
         weather_df = pd.DataFrame(weather_response['hourly'], columns = ['time'] + weather_params)
-        
+
         weather_df.to_csv(path, index=False)
-    
+
     print('Done ✅')
     return weather_df
+
+def get_energy_production(limit, offset, refine):
+
+    # create path or overwrite path --> see function caio for now we just request the data
+
+    #params to pass into the requests
+    params = {'limit': limit, 'offset': offset, 'refine': f'libelle_region:{refine}'}
+
+    #request the API for the data from 2013-2022
+    url_2013_2022 = 'https://odre.opendatasoft.com/api/v2/catalog/datasets/eco2mix-regional-cons-def/exports/json'
+
+    response_2013_2022 = requests.get(url=url_2013_2022,params = params).json()
+
+    #transform API request into a dataframe
+    df_2013_2022 = pd.DataFrame(response_2013_2022)
+
+    #request the API for the data from 2022-today
+    url_2022_today = 'https://odre.opendatasoft.com/api/v2/catalog/datasets/eco2mix-regional-tr/exports/json'
+
+    response_2022_today = requests.get(url=url_2022_today,params = params).json()
+
+    #transform API request into a dataframe
+    df_2022_today = pd.DataFrame(response_2022_today)
+
+
+    #merge those two together on just columns that exist in the first one
+    df_final = pd.concat([df_2013_2022, df_2022_today], sort=False,join="inner")
+
+    #transform the column "date_heure", so that it is compatible with the weather data
+    df_final.insert(0, "time", df_final['date'] + ' ' + df_final['heure'])
+    df_final['time'] =  pd.to_datetime(df_final['time'])
+    df_final = df_final.sort_values('time')
+
+    return df_final
