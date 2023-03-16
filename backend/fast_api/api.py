fe25117e862d4797ce9421462121ef8af0bd60d4
Jonathan Delrue <delrue.jonathan@gmail.com>
2023-03-16 17:22:47 -0300

    api predict

--- backend/fast_api/api.py
+++ backend/fast_api/api.py
@@ -1,35 +1,19 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 
-from get_data import WeatherEnergy
 from feature_processing import FeaturePreprocessing
 import tensorflow as tf
-
+from prophecy.get_data_forecast import WeatherForecast
 app = FastAPI()
-
-# # Allow all requests (optional, good for development purposes)
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],  # Allows all origins
-#     allow_credentials=True,
-#     allow_methods=["*"],  # Allows all methods
-#     allow_headers=["*"],  # Allows all headers
-# )
 
 @app.get("/")
 def index():
     return {"status": "ok"}
 
-@app.post('/predict')
-async def predict(img: UploadFile=File(...)):
-
-    #df = WeatherEnergy(
-    #    limit=-1, offset=0, refine='Hauts-de-France', city=['Heudicourt', 'Bucy-les-Pierrepont', 'Riencourt'], years=10, target='eolien'
-    #    ).merged()
-    #df_processed = FeaturePreprocessing(df).get_period_day()
-
-    new_data = FeaturePreprocessing(new_data).get_period_day()
-
+@app.get('/predict')
+def predict(city, days):
+    new_data = WeatherForecast(city, days)
+    processed_data = FeaturePreprocessing(new_data).get_period_day()
     new_model = tf.keras.models.load_model('saved_model/my_model')
 
     print(new_model.summary())
