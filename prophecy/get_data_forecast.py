d7d5c3ca1cdb62e1e13fef33b73149c5ee341e40
Tobias Rustler <Tobias.Rustler@outlook.de>
2023-03-17 16:57:54 -0300

    fixed the get_forexast file and now we can request data

--- prophecy/get_data_forecast.py
+++ prophecy/get_data_forecast.py
@@ -1,17 +1,15 @@
 import pandas as pd
 import requests
 from datetime import date
-from dateutil.relativedelta import relativedelta
 from geopy.geocoders import Nominatim
+from datetime import datetime,timedelta
 
-#
-# See the notebook "notebooks/Jonathan_notebook_weatherforecast.ipynb"
-#
+
+#rewrite to accept list (if we need to improve)
 
 class WeatherForecast:
-    def __init__(self, city:str, days:int):
+    def __init__(self, city:str):
         self.city = city
-        self.days = days
 
     def get_city_lonlan(self):
         '''
@@ -55,25 +53,17 @@ class WeatherForecast:
                       'shortwave_radiation','direct_radiation','direct_normal_irradiance',
                       'diffuse_radiation','windspeed_10m','windspeed_120m',
                       'winddirection_10m','winddirection_120m','windgusts_10m',
-                      'et0_fao_evapotranspiration','vapor_pressure_deficit',
+                      'et0_fao_evapotranspiration','weathercode','vapor_pressure_deficit',
                       'soil_temperature_0cm','soil_temperature_6cm',
                       'soil_temperature_18cm','soil_temperature_54cm',
-                      'soil_moisture_0_1cm','soil_moisture_1_3cm',
-                      'soil_moisture_3_9cm','soil_moisture_9_27cm', 'soil_moisture_27_81cm']
+                      'soil_moisture_0_1cm','soil_moisture_3_9cm','soil_moisture_9_27cm',
+                      'soil_moisture_27_81cm']
 
         # Then we compute the dates used to get the weather forecast data
 
-        # end_date : today + number of days of forecast (self.days)
-        end_date = (date.today() + relativedelta(days=self.days)).strftime('%Y-%m-%d')
-
-        #start_date : today at midnight
-        start_date = (date.today()).strftime('%Y-%m-%d')
 
         #call the method to receive the coordinates from the self.city list
         coordinates = self.get_city_lonlan()
-
-        #create an empty dataframe
-        # cities = []
 
         #create a dataframe with weather params for the selected city and store it
         lat = coordinates[self.city][0]
@@ -84,14 +74,18 @@ class WeatherForecast:
         weather_forecast_response= requests.get('https://api.open-meteo.com/v1/forecast',
                             params = {'latitude': lat,
                                         'longitude': lan,
-                                        'forecast_days' : self.days,
+                                        'forecast_days' : 2,
                                         'hourly': weather_params,
                                         'timezone': 'auto'}).json()
 
         weather_forecast_df = pd.DataFrame(weather_forecast_response['hourly'], columns = ['time'] + weather_params)
         weather_forecast_df['time'] = pd.to_datetime(weather_forecast_df['time'], format='%Y-%m-%d')
+        #get the start and end time
+        start_time = datetime.now()
+        end_time = start_time + timedelta(hours=24)
+        weather_forecast_df = weather_forecast_df[(weather_forecast_df['time'] >= start_time) & (weather_forecast_df['time'] < end_time)]
+        #set time as index
         weather_forecast_df = weather_forecast_df.set_index('time')
-
         # Format float to 1 decimal, sum the 3 tables and return the average
         pd.options.display.float_format = "{:,.1f}".format
 
@@ -101,14 +95,14 @@ class WeatherForecast:
     def rename_columns(self):
         weather_df_old_columns_names = self.get_weather_forecast()
 
-        weather_params_history_data = ["time",'temperature_2m','relativehumidity_2m','dewpoint_2m',
+        weather_params_history_data = ['temperature_2m','relativehumidity_2m','dewpoint_2m',
                       'apparent_temperature','pressure_msl','surface_pressure',
                       'precipitation','rain','snowfall','cloudcover',
                       'cloudcover_low','cloudcover_mid','cloudcover_high',
                       'shortwave_radiation','direct_radiation','direct_normal_irradiance',
                       'diffuse_radiation','windspeed_10m','windspeed_100m',
                       'winddirection_10m','winddirection_100m','windgusts_10m',
-                      'et0_fao_evapotranspiration','vapor_pressure_deficit',
+                      'et0_fao_evapotranspiration','weathercode','vapor_pressure_deficit',
                       'soil_temperature_0_to_7cm','soil_temperature_7_to_28cm',
                       'soil_temperature_28_to_100cm','soil_temperature_100_to_255cm',
                       'soil_moisture_0_to_7cm','soil_moisture_7_to_28cm',
