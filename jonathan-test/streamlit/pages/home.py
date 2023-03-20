11a5404766c5a568cfe7c5259ac9e6b543508aa6
Jonathan Delrue <delrue.jonathan@gmail.com>
2023-03-20 15:38:00 -0300

    works on streamlit

--- jonathan-test/streamlit/pages/home.py
+++ jonathan-test/streamlit/pages/home.py
@@ -27,4 +27,46 @@ def intro():
 
     st.sidebar.success('What would you like to do ?')
 
+st.markdown("<h1 style='text-align: center; color: red;'>Homepage</h1>", unsafe_allow_html=True)
+
 intro()
+
+img_link = {"unhappy" : '/home/jonathand/code/Johnny4good/electroprophet/jonathan-test/streamlit/images/face_unhappy.png',
+            "neutral" : '/home/jonathand/code/Johnny4good/electroprophet/jonathan-test/streamlit/images/face_neutral.png',
+            "happy" : '/home/jonathand/code/Johnny4good/electroprophet/jonathan-test/streamlit/images/face_happy.png'}
+
+
+col0, col1, col2, col3 = st.columns(4)
+
+img_width = 100
+
+with col0:
+    st.header("H+1")
+    st.image(img_link['happy'], width=img_width)
+    st.write('')
+    st.header("H+4")
+    st.image(img_link['unhappy'], width=img_width)
+
+
+with col1:
+    st.header("H+2")
+    st.image(img_link['neutral'], width=img_width)
+    st.write('')
+    st.header("H+6")
+    st.image(img_link['unhappy'], width=img_width)
+
+with col2:
+    st.header("H+3")
+    st.image(img_link['happy'], width=img_width)
+    st.write('')
+    st.header("H+7")
+    st.image(img_link['unhappy'], width=img_width)
+
+with col3:
+    st.header("H+4")
+    st.image(img_link['neutral'], width=img_width)
+    st.write('')
+    st.header("H+8")
+    st.image(img_link['neutral'], width=img_width)
+
+#st.image('streamlit/images/Contact-us.png', width=150)
